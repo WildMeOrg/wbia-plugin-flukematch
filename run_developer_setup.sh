706e67f2fd7e7aeb0c1ea3f110d143c8cefcbafc
tsubramanian <tamilselvan@gmail.com>
2023-08-08 21:36:30 +0530

    Update run_developer_setup.sh

--- run_developer_setup.sh
+++ run_developer_setup.sh
@@ -7,6 +7,9 @@ python setup.py clean
 
 pip install -r requirements.txt
 
+pip install -r https://raw.githubusercontent.com/Lasagne/Lasagne/master/requirements.txt
+pip install https://github.com/Lasagne/Lasagne/archive/master.zip
+
 python setup.py develop
 
 pip install -e .
